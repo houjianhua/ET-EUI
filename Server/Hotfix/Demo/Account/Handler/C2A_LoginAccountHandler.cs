50c50cdd645366d804ab991abd67213a3979c1d3
houjianhua <441834706@qq.com>
2022-04-15 18:47:46 +0800

    16课

---
+++ Server/Hotfix/Demo/Account/Handler/C2A_LoginAccountHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET
+{
+    public class C2A_LoginAccountHandler : AMRpcHandler<C2A_LoginAccount, A2C_LoginAccount>
+    {
+        protected override async ETTask Run(Session session, C2A_LoginAccount request, A2C_LoginAccount response, Action reply)
+        {
+
+            await ETTask.CompletedTask;
+        }
+    }
+}
